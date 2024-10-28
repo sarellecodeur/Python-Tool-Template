85f992a581aabca7c0ea466115a5df7acad5b902
sarellecodeur <darrelstampflibarri@gmail.com>
2024-10-28 08:37:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("TEMPLATE TOOL INSERT AN ASCII ART HERE")
+def main():
+    n = input ("1- Option1 \n2- Option2 \n3- Option3 \n\nPlease enter a number : ")
+
+if __name__ == "__main__" :
+    main()
+
+# INSERT THE CODE OF THE PLUGIN U WANT HERE
